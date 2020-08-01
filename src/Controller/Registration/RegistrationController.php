1ed77837454fc8d76b53f1df743e51cc4b97689f
unknown <houssemeddinesouissi10@gmail.com>
2020-08-01 12:13:14 +0100

    fix: email sender

--- src/Controller/Registration/RegistrationController.php
+++ src/Controller/Registration/RegistrationController.php
@@ -50,7 +50,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('Houssemeddinsouissi10@gmail.com', 'DevBrains404 Team'))
+                    ->from(new Address('DevBrains404@gmail.com', 'DevBrains404 Team'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
